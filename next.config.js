992d1da3b400347fa746fad3f6319dfa10fa0049
Caloganathan <caloganathan@gmail.com>
2025-09-26 23:46:20 +0700

    Fix: Add missing i18n configuration files for bilingual support

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
   },
